683e87b8a5c9c6ed18bbb7a82dab2448d0d5ed18
ulgoon <ulgoon89@gmail.com>
2022-05-17 16:08:00 +0900

    feat: Set player_turn_score, com_turn_score

--- CLI/pig-dice-game.py
+++ CLI/pig-dice-game.py
@@ -1,2 +1,15 @@
 print('pig dice game start!')
 scoreboard = [0, 0]
+
+print(scoreboard)
+
+player_turn_score = 0
+com_turn_score = 0
+
+player_turn_score += 1
+com_turn_score += 1
+
+scoreboard[0] += player_turn_score
+scoreboard[1] += com_turn_score
+
+print(scoreboard)
